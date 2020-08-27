a97c1a373b2920dfbc86343feb06ce92bb7a0433
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-08-26 21:10:31 -0400

    Code now recognizes the ends of setences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.reverse.chr == "."
   end
 
   def question?
-
+    self.reverse.chr == "?"
   end
 
   def exclamation?
-
+    self.reverse.chr == "!"
   end
 
   def count_sentences
+    count = 0
+    index = 0
+    string_to_array = self.split("")
+    string_to_array.each do |letter|
+      if (string_to_array[index - 1] != letter) && (letter == "." || letter == "?" || letter == "!")
+        count += 1
+      end
 
+      index += 1
+    end
+    count
   end
 end
+
+
